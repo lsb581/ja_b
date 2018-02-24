737542c4491499b36e4f44b08078935abf7a35a5
lsb581 <36673971+lsb581@users.noreply.github.com>
2018-02-24 04:21:03 +0000

    tree1/2-(2)_ex
    .

---
+++ (반대방향)반잘린트리a20171214_05.java
@@ -0,0 +1,17 @@
+public class a20171214_05{
+public static void main(String args[]){
+int a,b,c;
+
+ for(a=1; a<10;a++) {
+ for(b=9-a; b>0; b--){
+System.out.print(" ");
+ }
+ for(c=a; c>0; c--){
+System.out.print("*");
+ }
+ System.out.println("");
+}
+
+
+}
+}
