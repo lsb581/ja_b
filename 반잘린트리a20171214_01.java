1d8f0c4b38de681488e2c86901c1d1c77d4bc4c8
lsb581 <36673971+lsb581@users.noreply.github.com>
2018-02-24 04:20:11 +0000

    tree1/2_ex
    .

---
+++ 반잘린트리a20171214_01.java
@@ -0,0 +1,13 @@
+public class a20171214_01{
+public static void main(String args[]){
+/*반 잘린 트리*/
+for(int a=1; a<=10; a++){
+ for(int b=1; b<=a; b++){
+System.out.print("*");
+}
+System.out.println("");
+}
+
+
+}
+}
