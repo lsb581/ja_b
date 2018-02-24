a76961190980a1a735cc7625714c9afcd9f95d4b
lsb581 <36673971+lsb581@users.noreply.github.com>
2018-02-24 04:16:16 +0000

    calender_ex
    .

---
+++ 1월부터12월a20171214_02.java
@@ -0,0 +1,13 @@
+public class a20171214_02{
+public static void main(String args[]){
+/*1월 1일부터 12월 31일까지 출력*/
+for(int a=1; a<=12; a++){
+ for(int b=1; b<=31; b++){
+System.out.println(a+"월"+b+"일");
+}
+
+}
+
+
+}
+}
